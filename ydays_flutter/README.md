274635e1da2fd25ecab668426e357531e9c8127b
Timothee <timothee.sylla@ynov.com>
2025-11-26 10:27:31 +0100

    init-flutter

---
+++ ydays_flutter/README.md
@@ -0,0 +1,3 @@
+# ydays_flutter
+
+A new Flutter project.
---
+++ ydays_flutter/android/app/src/main/kotlin/com/example/ydays_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ydays_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
